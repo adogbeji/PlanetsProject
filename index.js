f1d8ef957f254afa7d4ea5495b59d261fd7f35de
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2022-12-06 14:27:39 +0000

    Add function for filtering out habitable planets

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const { parse } = require('csv-parse');
 const fs = require('fs');
 
-const results = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
 
 fs.createReadStream('kepler_data.csv')
 .pipe(parse({
@@ -9,7 +15,9 @@ fs.createReadStream('kepler_data.csv')
     columns: true
 }))
 .on('data', (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+        habitablePlanets.push(data);
+    }
 })
 .on('error', (err) => {
     console.log(err);
